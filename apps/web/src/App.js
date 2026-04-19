eeff8a008ad9dd4a5d1c17edddfa4970e853275d
Travis Collins <travisfcollins@gmail.com>
2026-04-19 18:29:35 -0400

    chore(web): remove build artifacts from git, extend .gitignore

--- apps/web/src/App.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { Heading } from '@chakra-ui/react';
-export function App() {
-    return _jsx(Heading, { p: 6, children: "Prism" });
-}
--- apps/web/src/theme.js
+++
@@ -1,6 +0,0 @@
-import { createSystem, defaultConfig } from '@chakra-ui/react';
-export const system = createSystem(defaultConfig, {
-    globalCss: {
-        'html, body': { backgroundColor: '#0f1419', color: '#e2e8f0' },
-    },
-});
